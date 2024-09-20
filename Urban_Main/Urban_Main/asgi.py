803c357ee64c07d6ccae622602ca8215b05f3f70
kavya <setavakavya3@gmail.com>
2024-09-20 20:46:46 +0530

    asgi import

--- Urban_Main/Urban_Main/asgi.py
+++ Urban_Main/Urban_Main/asgi.py
@@ -7,12 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
+
+from channels.auth import AuthMiddlewareStack
+
 import os
 
 from django.core.asgi import get_asgi_application
-
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+
 
 
 
