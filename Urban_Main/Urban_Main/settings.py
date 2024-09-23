003f77368452fd5e64fb807b9c6e37c7d1f42516
neeraj <neeraj.automac@gmail.com>
2024-09-23 12:05:59 +0530

    accesstoken life time to 1 day

--- Urban_Main/Urban_Main/settings.py
+++ Urban_Main/Urban_Main/settings.py
@@ -88,8 +88,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=6),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 'ALGORITHM': 'HS256',
